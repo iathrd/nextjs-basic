57780020479d943be38335f06086a08824d69dd4
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-12-23 17:39:53 +0700

    nested dynamic routes

---
+++ pages/product/[productid]/review/[reviewid].js
@@ -0,0 +1,11 @@
+import {useRouter} from 'next/router';
+
+function Review(){
+
+    const router = useRouter()
+    const {productid,reviewid} = router.query
+    return <h1>Review {reviewid} for product {productid} </h1>
+
+}
+
+export default Review;
