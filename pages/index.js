fdeb117272df6efd434e1ba4784cb3a0593ca09e
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-12-25 11:03:01 +0700

    navigation programmatically

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('placing your order')
+    router.push('/product')
+  }
+
   return (
    <>
    <h1>Home Page</h1>
    <Link href="/blog" >
    <a>Blog Page</a>
    </Link>
+   <button onClick={handleClick} > Place Order</button>
    </>
   )
 }
