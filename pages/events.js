22e2ad88dddc15ac45f227d888805883f96267f5
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-01-05 10:50:50 +0700

    shallow routing

--- pages/events.js
+++ pages/events.js
@@ -1,15 +1,16 @@
 import { useState } from "react"
+import {useRouter} from 'next/router';
 
 function EventList({eventList}) {
-
     const [events, setEvents] = useState(eventList)
+    const router = useRouter()
 
 
     const fetchSportEvents = async () => {
         const response = await fetch('http://localhost:4000/events?category=sports')
         const data = await response.json()
-
         setEvents(data)
+        router.push('/events?category=sports',undefined,{shallow:true})
     }
 
     return (
@@ -33,8 +34,11 @@ function EventList({eventList}) {
 
 export default EventList
 
-export async function getServerSideProps(){
-    const response = await fetch('http://localhost:4000/events')
+export async function getServerSideProps(context){
+    const {query} = context
+    const {category} = query
+    const queryString = category ? 'category=sports' : ''
+    const response = await fetch(`http://localhost:4000/events ? ${queryString}`)
     const data = await response.json()
 
     return {
