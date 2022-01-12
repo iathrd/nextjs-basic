842321522ee920f5f38a4da555d79129d39f2f18
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-01-12 19:11:33 +0700

    App Layout

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,16 @@
+import Footer from "../components/Footer";
 
 function About(){
-    return <h1>About Page</h1>
+    return <h1 className="content">About Page</h1>
 }
 
 export default About;
+
+About.getLayout = function PageLayout(page) {
+    return (
+        <>
+        {page}
+        <Footer/>
+        </>
+    )
+}
