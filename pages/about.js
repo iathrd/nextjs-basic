3165e2ac3ec4bd731be103bd96cb764c4f0c2f82
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-01-12 19:20:40 +0700

    Head Component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import Footer from "../components/Footer";
 
 function About(){
-    return <h1 className="content">About Page</h1>
+    return (
+        <>
+        <Head>
+            <title>About Iqbal Athorid</title>
+            <meta name="description" content="About iqbal" ></meta>
+        </Head>
+    <h1 className="content">About Page</h1>
+    </>
+    )
 }
 
 export default About;
